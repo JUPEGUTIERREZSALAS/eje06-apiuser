5dfa3fef8193cc9abbedc83b1f5bae4e043a658a
Juan Pedro Gutierrez Salas <jupegutierrezsa@ittepic.edu.mx>
2017-10-13 18:32:08 -0600

    V0.9

--- router/user.router.js
+++ router/user.router.js
@@ -3,8 +3,8 @@ const handler = require('../utils/handler');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/unidad02', {
-  useMongoClient: true
+mongoose.connect('mongodb://localhost:27017/unidad2', {
+    useMongoClient: true
 });
 
 const User = require('../models/user.model');
@@ -13,16 +13,70 @@ module.exports = () => {
 
     router.get('/', (req, res) => {
         User.find({})
-        .sort()
-        .exec(handler.handleMany.bind(null, 'users', res));
+            .sort()
+            .exec(handler.handleMany.bind(null, 'users', res));
     });
 
     router.get('/:id', (req, res) => {
         const id = req.params.id;
-        User.find({_id:id})
-        .sort()
-        .exec(handler.handleMany.bind(null, 'users', res));
+        User.find({ _id: id })
+            .sort()
+            .exec(handler.handleOne.bind(null, 'users', res));
     });
+
+    //Consulta de Nombre
+    router.get('/nombre/:name', (req, res) => {
+        const name = req.params.name;
+        User.find({ name: name })
+            .sort()
+            .exec(handler.handleOne.bind(null, 'users', res));
+    });
+
+    //Consulta de email
+    router.get('/correo/:correo', (req, res) => {
+        const correo = req.params.correo;
+        User.find({ email: correo })
+            .sort()
+            .exec(handler.handleOne.bind(null, 'users', res));
+    });
+
+    // Inserciones
+    router.post('/', (req, res) => {
+        // Recibir parametros
+        const usuario = req.body;
+
+        User.create(usuario)
+            .then(
+                function(data) {
+                    console.log(data);
+                    res.json(data);
+                }
+            )
+            .catch(
+                function(err) {
+                    console.log(err);
+                    res.status(400);
+                    res.json(err);
+                }
+            );
+    });
+
+    // Actualizaciones
+    router.delete('/:id', (req, res) => {
+        // Recibir parametros
+        const id = req.params.id;
+
+        User.remove({ _id: id }, function(err, data) {
+            if (err) {
+                console.log(err);
+                res.status(400);
+                res.json({ error: err });
+            } else {
+                res.json({ msj: "Todo estuvo bien" });
+            }
+        });
+    });
+
 
     return router;
 }
